6cc5b8b3654c1eb5a274ebae4e101b77f2af36d8
Adel Mahmoud <mahmoud.adel@ynov.com>
2026-05-12 00:41:30 +0200

    app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,20 @@
-// Install la dependance Express pour le serveur
-
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8080
+// Permettre la discussions entre fichier
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Relier les routes avec app.js
+app.use("/router", require("./router/routes"));
+
+// relier le frontend avec le serveur
+app.use(express.static(path.join(__dirname, '../frontend')));
+// lancer le serveur
+app.listen(port, () => {
+    console.log(`Serveur listening on port ${port}`)
+});
